c64b447f283f3907a86565761aacff79028aab80
Ayush Karn <ayush.karn@hp.com>
2024-06-08 19:05:33 +0530

    changes for dark more and footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,8 @@
 import React, { FC } from "react";
 import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram, faFacebookF, faTiktok, faBehance } from '@fortawesome/free-brands-svg-icons';
+
 
 interface FooterProps {}
 
@@ -15,70 +18,81 @@ const Footer: FC<FooterProps> = () => (
         style={{ fill: "transparent", width: "100%", height: "100%" }}
       >
         <path
-          fill="#ede0e0b0"
+          style={{ fill: "var(--light-background-color)" }}
           fill-opacity="1"
           d="M0,160L48,144C96,128,192,96,288,101.3C384,107,480,149,576,144C672,139,768,85,864,74.7C960,64,1056,96,1152,117.3C1248,139,1344,149,1392,154.7L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
         ></path>
         <path
-          fill="#1a1818dd"
+          style={{ fill: "var(--dark-background-color)" }}
           fill-opacity="1"
           d="M0,160L48,144C96,128,192,96,288,101.3C384,107,480,149,576,144C672,139,768,85,864,74.7C960,64,1056,96,1152,117.3C1248,139,1344,149,1392,154.7L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
         ></path>
       </svg>
     </div>
-    <div className="Footer">
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4">
-        <div className="footer-trademark">
-          <div className="footer-logo">
+    <div className="Footer py-3 ">
+      <div className="footer-left">
+        <div className="footer-logo">
           <img
-                  alt=""
-                  src="/assets/logo.png"
-                  width="50"
-                  height="50"
-                  className="d-inline-block align-top"
-                />
+            alt=""
+            src="/assets/logo.png"
+            width="50"
+            height="50"
+            className="d-inline-block align-top"
+          />
+        </div>
+        <div className="footer-design-trademark">
+          <div className="footer-design footer-text">
+            Crafted with care by Ayush
           </div>
-          <div className="footer-text col-md-4 mb-0">
+          <div className="footer-trademark footer-text">
             &copy; {currentYear} The Arts Diary
           </div>
         </div>
+      </div>
 
-        <div className="footer-image-container">
-          <img
-            src="/assets/footer_brush_2.png"
-            alt="Decorative"
-            className="footer-image"
-          />
+      <div className="footer-image-container">
+        <img
+          src="/assets/footer_brush_2.png"
+          alt="Decorative"
+          className="footer-image"
+        />
+      </div>
+
+      <div className="footer-right">
+        <div className="footer-follow">
+          <a href="https://instagram.com" className="social-icon">
+          <FontAwesomeIcon icon={faInstagram}/>
+          </a>
+          <a href="https://facebook.com" className="social-icon">
+          <FontAwesomeIcon icon={faFacebookF} />
+          </a>
+          <a href="https://tiktok.com" className="social-icon">
+          <FontAwesomeIcon icon={faBehance} />
+          </a>
+          <a href="https://behance.net" className="social-icon">
+          <FontAwesomeIcon icon={faTiktok} />
+          </a>
         </div>
-
-        <ul className="footer-nav col-md-4 justify-content-end">
-          <li className="footer-nav-item">
-            <a href="#" className="footer-nav-link px-2">
-              Home
-            </a>
-          </li>
-          <li className="footer-nav-item">
-            <a href="#" className="footer-nav-link px-2">
-              Features
-            </a>
-          </li>
-          <li className="footer-nav-item">
-            <a href="#" className="footer-nav-link px-2">
-              Pricing
-            </a>
-          </li>
-          <li className="footer-nav-item">
-            <a href="#" className="footer-nav-link px-2">
-              FAQs
-            </a>
-          </li>
-          <li className="footer-nav-item">
-            <a href="#" className="footer-nav-link px-2">
-              About
-            </a>
-          </li>
-        </ul>
-      </footer>
+        <div className="footer-quicklink">
+          <ul className="footer-nav">
+            <li className="footer-nav-item">
+              <a href="#" className="footer-nav-link">
+                Terms of Service
+              </a>
+            </li>
+            <li className="footer-nav-item">
+              <a href="#" className="footer-nav-link">
+                Privacy Policy
+              </a>
+            </li>
+            <li className="footer-nav-item">
+              <a href="#" className="footer-nav-link">
+                Sitemap
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 );
