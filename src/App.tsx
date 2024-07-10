f581303771df17a1947b44d6fda167e89da4a28f
Ayush Karn <ayush.karn@hp.com>
2024-07-10 21:59:35 +0530

    test changes with browserroute

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Orders from "./components/Orders/Orders";
 import Blogs from "./components/Blogs/Blogs";
